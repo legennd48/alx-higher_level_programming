bffd4cfdcb6c50517caa26b895b3c8a7935ea9e6
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-07 22:45:35 +0000

    initial commit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    av = sys.argv
+    ac = len(av) - 1
+
+    if ac != 3:
+        print("{}".format("Usage: ./100-my_calculator.py <a> <operator> <b>"))
+        exit(1)
+
+    else:
+        a = int(av[1])
+        op = av[2]
+        b = int(av[3])
+
+        if op == '+':
+            print("{} + {} = {}".format(a, b, add(a, b)))
+
+        elif op == '-':
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+
+        elif op == '*':
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+
+        elif op == '/':
+            print("{} / {} = {}".format(a, b, div(a, b)))
+
+        else:
+            print("{}".format("Unknown operator. Available operators: +, -, * and /"))
+            exit(1)
