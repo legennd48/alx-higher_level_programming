48a0cb2857355b0b90e59886c20eeea3c90974c0
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-07 20:59:32 +0000

    initial commit

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    num = len(argv) - 1
+
+    if num <= 1:
+        print("{}".format(0 if num == 0 else 1))
+
+    else:
+        sum = 0
+        i = 1
+        while i <= num:
+            sum += int(argv[i])
+            i += 1
+
+    print("{}".format(sum))
