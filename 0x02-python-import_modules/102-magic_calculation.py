f62e93c2bc21454c33b2e449b63162ac4687dcd3
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-07 23:00:32 +0000

    fixed indentation error

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -8,9 +8,10 @@ def magic_calculation(a, b):
 
         for i in range(4, 6):
             c = add(c, i)
-            return (c)
+
+        return (c)
 
     else:
         return (sub(a, b))
 
-    #magic_calculation_102(None, None)
+magic_calculation_102(None, None)
