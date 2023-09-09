f4a9f2e0e89401490aeacb780df9b99e1f5922a8
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-09 07:41:05 +0000

    changed logic

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,10 +3,6 @@
 
 def print_reversed_list_integer(my_list=[]):
     max_idx = len(my_list) - 1
-    if max_idx == -1:
-        print("[]")
-
-    else:
-        while max_idx >= 0:
-            print("{:d}".format(my_list[max_idx]))
-            max_idx -= 1
+    while max_idx >= 0:
+        print("{:d}".format(my_list[max_idx]))
+        max_idx -= 1
