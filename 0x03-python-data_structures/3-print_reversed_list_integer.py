b20d7eeff58f914119f4f35f30ca85ad4caaec5c
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-09 07:44:13 +0000

    added condition to hande none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    max_idx = len(my_list) - 1
-    while max_idx >= 0:
-        print("{:d}".format(my_list[max_idx]))
-        max_idx -= 1
+    if my_list is not None:
+        max_idx = len(my_list) - 1
+        while max_idx >= 0:
+            print("{:d}".format(my_list[max_idx]))
+            max_idx -= 1
