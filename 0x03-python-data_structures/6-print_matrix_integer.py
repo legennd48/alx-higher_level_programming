1f910cb1fe840b0a72588260e63db9e2c3658544
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-09 07:22:25 +0000

    initial commit

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            print("{}".format(i), end=" " if i != row[:-1] else "\n")
+
+        print()
