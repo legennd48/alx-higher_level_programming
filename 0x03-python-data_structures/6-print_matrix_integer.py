8acd0524dd6cc255442ba95dcb4e5b3dadac14ce
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-12 14:17:11 +0000

    fixed for empty matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,4 +4,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i in row:
-            print("{:d}".format(i), end=" " if i != row[-1] else "\n")
+            print("{:d}".format(i), end=" " if i != row[-1] else "")
+
+        print()
