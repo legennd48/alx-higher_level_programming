35a19eab9be3ce28c81219802f64c63335a4bf46
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-13 20:32:41 +0000

    initial commit

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def roman_to_integer(roman_string):
+    if roman_string is None or type(roman_string) is not str:
+        return (0)
+
+    if roman_string == "":
+        return (0)
+
+    num = 0
+    roman_dict = {
+        "I": 1, "V": 5, "X": 10, "L": 50,
+        "C": 100, "D": 500, "M": 1000
+    }
+
+    for i, j in zip(roman_string, roman_string[1:]):
+        if i not in roman_dict:
+            return (0)
+        elif roman_dict[i] >= roman_dict[j]:
+            num += roman_dict[i]
+        else:
+            num -= roman_dict[i]
+
+    num += roman_dict[roman_string[-1]]
+    return (num)
