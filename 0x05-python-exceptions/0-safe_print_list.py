64ebd935c6dd569d20347c3fcee742aecee9ff30
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-25 21:42:24 +0000

    initial commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     n = 0
     while (n < x):
         try:
-            print("{}".format(my_liat[n]), end="")
+            print("{}".format(my_list[n]), end="")
             n += 1
 
         except IndexError:
