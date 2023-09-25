56a7c38e31a1e38a7f3cdc96939161047065e70a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-25 23:21:47 +0000

    initial commit

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return (fct(*args))
+
+    except as err:
+
+        stderr.write("Exception: {}\n".format(err))
+
+    return (None)
---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+def your_function(a, b):
+
+    result = 0
+
+    for i in range(1, 3):
+        try:
+            if (i > a):
+                raise Exception('Too far')
+
+            result += (a ** b) / i
+
+        except Exception:
+            result += b + a
+
+    return (result)
