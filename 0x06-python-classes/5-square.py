068bbfa35302d1fc477b53503f3f933ae83980d4
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-27 03:43:37 +0000

    initial commit

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+''' class square with setter, getter, prints the shape andfinds the  area '''
+
+
+class Square:
+    ''' class defines a square. '''
+    def __init__(self, size=0):
+        ''' Instantiation of private instance attribute.
+        Args:
+        param1: size of the square which must be int
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        ''' returns the current square area'''
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        ''' retireves current size of square '''
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        ''' sets size of square '''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def my_print(self):
+        ''' prints the square using # '''
+        size = self.__size
+        if (size == 0):
+            print()
+
+        for i in range(size):
+            print("#" * size)
