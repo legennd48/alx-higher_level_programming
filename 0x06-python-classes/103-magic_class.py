93d4fe7e4dbf32278f2f1efdf60ae48633b4e691
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-09-27 04:50:13 +0000

    initial commit

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' code to match provided bytecode '''
+import math
+
+
+
+class MagicClass:
+    ''' class definition '''
+    def __init__(self, radius=0):
+        ''' initialization of rad'''
+        self.__radius = 0
+
+        if not isinstance(radius, (int, float)):
+            raise TypeError('radius must be a number')
+
+        self.__radius = radius
+
+    def area(self):
+        ''' returns area '''
+        return (2 * math.pi * self.__radius ** 2)
+
+    def circumference(self):
+        ''' calculates circumference '''
+        return (2 * math.pi * self.__radius)
