93a9ba61508fed04e55b0d97ffb1956c32631e4b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-03 05:42:45 +0000

    fix shebang

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,4 +1,4 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
 '''
 Module: 0-rectangle
 an empty class that defines a rectangle
---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python3
+"""
+Solves the N-queens puzzle.
+
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+
+Usage:
+     ./101-nqueens.py N
+
+Where N must be an integer greater than or equal to 4.
+
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
+
+import sys
+
+
+def is_safe(board, row, col, N):
+    # Check if there is a queen in the same column
+    for i in range(row):
+        if board[i][col] == 1:
+            return (False)
+
+    # Check upper left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return (False)
+
+    # Check upper right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, N)):
+        if board[i][j] == 1:
+            return (False)
+
+    return True
+
+def solve_nqueens_util(board, row, N):
+    if row == N:
+        print_solution(board)
+        return (True)
+
+    res = False
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row][col] = 1
+            res = solve_nqueens_util(board, row + 1, N) or res
+            board[row][col] = 0
+
+    return (res)
+
+def solve_nqueens(N):
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    if not solve_nqueens_util(board, 0, N):
+        print("No solution exists")
+        sys.exit(1)
+
+def print_solution(board):
+    for row in board:
+        print(" ".join(["Q" if cell else "." for cell in row]))
+    print()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solve_nqueens(N)
