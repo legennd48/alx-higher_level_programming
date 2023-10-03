f552693f8ba9ba110dc7bfb28af4f85b16e1dfec
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-03 23:25:38 +0000

    doc

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-class LockedClass:
+'''
+Module: 29. #pythonic
+Defines class with no class or object attribute
+Control dynamically created instance attributes
+'''
+
+class LockedClass():
+    '''
+    prevent user from creating new instance attribute dynamically
+    unless attribute is "first_name"
+    '''
+
     __slots__ = ["first_name"]
