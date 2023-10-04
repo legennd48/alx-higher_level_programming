370d1fac635e3b8bdae169188f96578b54b78c62
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-04 21:02:32 +0000

    simplified push

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -11,24 +11,4 @@ def lazy_matrix_mul(m_a, m_b):
     """
     Multiplies two matrices using NumPy and returns the result
     """
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
-    if not all(isinstance(row, list) for row in m_a):
-        raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
-    try:
-        np_result = np.matmul(np.array(m_a), np.array(m_b))
-        result = np_result.tolist()
-        return result
-    except ValueError as e:
-        raise ValueError("m_a and m_b can't be multiplied") from e
+    return (np.matmul(m_a, m_b))
