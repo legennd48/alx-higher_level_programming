64dd3ea4e12070bd89b56597f4a47d6c2b02a147
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-12 14:08:57 +0000

    documentation fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
 Module: 7. Load, add, save
-Contains function that adds and saves to Python obj to JSON file; loads objects
+adds all arguments to a Python list, and then save them to a file:
 """
 
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = \
-    __import__('6-load_from_json_file.py').load_from_json_file
+import sys
 
-filename = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
-
-save_to_json_file(existing_content + argv[1:], filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
