07677a2d3b2de50425a162a77176ec5e9bc630a5
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-16 18:05:37 +0000

    logic improvement

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -35,10 +35,9 @@ class Rectangle(Base):
         ''' width setter '''
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.__width = value
 
     @property
     def height(self):
@@ -50,10 +49,9 @@ class Rectangle(Base):
         ''' height setter '''
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.__height = value
 
     @property
     def x(self):
@@ -65,10 +63,9 @@ class Rectangle(Base):
         ''' x coordinate setter '''
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+        self.__x = value
 
     @property
     def y(self):
@@ -80,10 +77,9 @@ class Rectangle(Base):
         ''' coordinate 'y' setter '''
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
+        self.__y = value
 
     def area(self):
         ''' Clalculates the area of the Rectangle '''
