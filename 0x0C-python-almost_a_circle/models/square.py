973521620ba403a53490254100bd843b3d325a6b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-25 19:52:45 +0000

    TypeError and ValueError fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,6 +26,10 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         ''' setter for size '''
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
         self.height = value
         self.width = value
 
