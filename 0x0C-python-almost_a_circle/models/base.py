df14b1457ee22d27ee671e86cdca47b1a9d8aa74
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-10-25 17:05:41 +0000

    initial commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -134,3 +134,19 @@ class Base:
             drawing_turtle.hideturtle()
 
         turtle.exitonclick()
+
+    def load_from_file_csv(cls):
+        """Return a list of classes instantiated from a CSV file."""
+         filename = cls.__name__ + ".csv"
+        try:
+            with open(filename, "r", newline="") as csvfile:
+                if cls.__name__ == "Rectangle":
+                    fieldnames = ["id", "width", "height", "x", "y"]
+                else:
+                    fieldnames = ["id", "size", "x", "y"]
+                list_dicts = csv.DictReader(csvfile, fieldnames=fieldnames)
+                list_dicts = [dict([k, int(v)] for k, v in d.items())
+                              for d in list_dicts]
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
