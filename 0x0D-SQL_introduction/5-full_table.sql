9a3c968edbe0b6ce767a3093fdd58dde0659b1fc
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-14 12:09:29 -0800

    initial commit

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- shows the full description of table 'first_table'
+-- without DESCRIBE or EXPLAIN statements
+
+SHOW CREATE TABLE first_table;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- inserts a new row to table 'first_table
+
+INSERT INTO first_table
+VALUES (89, 'Best School');
