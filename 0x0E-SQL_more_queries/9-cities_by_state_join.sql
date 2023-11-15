2e3cdc3dd1e8d569c77c042c3b6440777a5b9069
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-15 14:18:15 -0800

    another typo

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,6 +5,5 @@
 
 SELECT cities.id, cities.name, states.name
 FROM cities
-JOIN states
-WHERE cities.states_id = states.id
+INNER JOIN states ON cities.states_id = states.id
 ORDER BY cities.id ASC;
