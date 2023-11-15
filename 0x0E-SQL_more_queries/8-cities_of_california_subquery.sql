b3e837b227453dfd018e7ec804ea110721b6eacd
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-15 14:15:00 -0800

    typo fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,5 @@
 --  lists all the cities of California that can be found in the database hbtn_0d_usa
 
 SELECT id, name FROM cities
-WHERE state_id = (SELECT id FROM states where name = 'California') GROUP BY id ORDER BY id ASC
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
+GROUP BY id ORDER BY id ASC;
