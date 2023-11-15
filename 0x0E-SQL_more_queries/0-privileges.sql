16bc701829b3a74a40e5fd69ee4945b6a2328368
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-15 12:29:05 -0800

    initial commit

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- lists all privilages of the MYSQL user user_0d_1 and user_0d_2
 
-SHOW PRIVILEGES FOR 'user_0d_1'@'localhost';
-
-SHOW PRIVILEGES FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
