e8956e41a4bc95624975b2bfbb8d3545c951558a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-15 14:59:19 -0800

    initial commit#

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,8 +1,9 @@
---  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+--  lists all shows contained in hbtn_0d_tvshows without a genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
