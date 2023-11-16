fccbfeac87185ab9f334f357c11272f514b38454
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-16 00:09:55 -0800

    small fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -3,8 +3,8 @@
 -- Each record should display: tv_shows.title - tv_genres.name
 -- Results must be sorted in ascending order by the show title and genre name
 
-SELECT tv_shows.title, tv_genres.name
+SELECT title, name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genre ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name ASC;
+ORDER BY title, name ASC;
