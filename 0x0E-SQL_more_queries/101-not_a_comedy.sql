e24b6a1247aa51184e928c15c21ce33a93ee0b9b
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-11-16 00:06:00 -0800

    typo fix

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,7 @@
 --  lists all shows without the genre Comedy in the database hbtn_0d_tvshows.
 
 -- The tv_genres table contains only one record where name = Comedy (but the id can be different)
---Each record should display: tv_shows.title
+-- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
 
 
