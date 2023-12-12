dbb26c2242c09046f004f284933a1a8b50426a47
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-12 23:02:35 +0000

    end of lline

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const txt1 = fs.readFileSync(process.argv[2]).toString();
-const txt2 = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], txt1 + '\n' + txt2 + '\n');
+const txt1 = fs.readFileSync(process.argv[2], 'utf8');
+const txt2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], txt1 + txt2);
