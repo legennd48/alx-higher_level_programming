1551adf3ec8f60226f37c8697cec031ad76bbfdd
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-12 00:50:48 +0000

    fixed typo

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg = parseInt(process.argv[2]);
-if (isNaN(arg) || arg === undefined) {
+if (isNaN(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My Number:', arg);
+  console.log('My number:', arg);
 }
