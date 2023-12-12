7db023bcf1c393cd0219cb882037a83b5f9996a9
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-12 22:50:29 +0000

    added new lines

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 
 const txt1 = fs.readFileSync(process.argv[2]).toString();
 const txt2 = fs.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], txt1 + txt2);
+fs.writeFileSync(process.argv[4], txt1 + '\n' + txt2 + '\n');
