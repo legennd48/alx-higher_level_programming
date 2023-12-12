2c79d9188d4c562eee1606b960dd20be456b9964
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-12 00:17:46 +0000

    initial commikt and style fix

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 const arg = process.argv.slice(2);
 if (arg[0] === undefined) {
-    console.log('No argument');
+  console.log('No argument');
 } else {
   console.log(arg[0]);
 }
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const arg1 = process.argv.slice(2);
+const arg2 = process.argv.slice(3);
+
+console.log(arg1[0], 'is', arg2[0]);
