fe4b34b1f99ab3e8beba7a7a412240844ecd31c8
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-12 22:44:10 +0000

    logic improvement

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-const firstFilePath = process.argv[2];
-const secondFilePath = process.argv[3];
-const outputFilePath = process.argv[4];
-
-const contentFromFirstFile = fs.readFileSync(firstFilePath, 'utf8');
-const contentFromSecondFile = fs.readFileSync(secondFilePath, 'utf8');
-const concatenatedContent = contentFromFirstFile + contentFromSecondFile;
-
-fs.writeFileSync(outputFilePath, concatenatedContent);
+const txt1 = fs.readFileSync(process.argv[2]).toString();
+const txt2 = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], txt1 + txt2);
