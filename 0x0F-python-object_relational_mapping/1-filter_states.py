0f706d631d7fc52385359b8a4320c257e7d80996
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-15 17:12:41 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''
+Module: 1. Filter states
+script that lists all states with a name starting with N (upper N)
+'''
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    userName = sys.argv[1]
+    pw = sys.argv[2]
+    dbName = sys.argv[3]
+    host = 'localhost'
+    port = 3306
+
+    # connection to db
+    conn = MySQLdb.connect(host=host, port=port, user=userName,
+                           passwd=pw, db=dbName)
+    # creating cursor
+    cursor = conn.cursor()
+    # query
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    # execute query
+    cursor.execute(query)
+
+    # get results
+    results = cursor.fetchall()
+
+    # print results
+    for row in results:
+        print(row)
+
+    # close cursor and connection
+    cursor.close()
+    conn.close()
