305a899321566199fbfbdb2e3b779b4dc78cad99
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-15 19:45:21 +0000

    initial commit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'''
+Module: 4. Cities by states
+lists all cities from the database hbtn_0e_4_usa
+'''
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    host = 'localhost'
+    port = 3306
+    userName = argv[1]
+    pw = argv[2]
+    dbName = argv[3]
+
+    conn = MySQLdb.connect(host=host, port=port, user=userName, passwd=pw,
+                           db=dbName)
+    cursor = conn.cursor()
+    qry = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.id = states.id
+    ORDER BY cities.id ASC
+    """
+    cursor.execute(qry)
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+
+    cursor.close()
+    conn.close()
