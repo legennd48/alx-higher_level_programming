af190ecc56f002c767ee9ab2120be016c0b294ea
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2023-12-16 00:46:48 +0000

    change of filter

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -21,8 +21,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    instance = session.query(State).filter(
-        State.name.like('%{}%'.format(find))).first()
+    instance = session.query(State).filter_by(name='{}'.format(find)).first()
     if instance:
         print('{}'.format(instance.id))
     else:
