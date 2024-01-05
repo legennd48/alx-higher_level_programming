c68003fd9e4fa4777f73a08b3552acaff9bde49a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-05 14:04:31 +0000

    shellcheck and requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
-# Script takes an argument (URL) and displays the size of the
-# body of the responce in bytes
-
-url=$1
-
-curl -sI $url | grep Content-Length | cut -d' ' -f2
+# Script takes an argument (URL) and displays the size of the body in bytes
+curl -sI "$1" | grep Content-Length | cut -d' ' -f2
