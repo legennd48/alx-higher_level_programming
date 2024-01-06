351c643951104a485efcaf0f8072c7868db9daa7
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-06 17:12:24 +0000

    initial commit

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+script that takes in a URL, sends a request to
+the URL and displays the value of the variable
+X-Request-Id in the response header
+'''
+from sys import argv
+import requests
+
+
+url = argv[1]
+req = requests.get(url)
+x_id = req.headers.get('X-Request-Id')
+print("{}".format(x_id))
