7856d1e184c7f9a01bfca542f72cdcaac5af76e7
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-01-30 20:28:16 +0000

    initial commit

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,12 +3,12 @@
 
 const fs = require('fs');
 const filePath = process.argv[2];
+const content = process.argv[3];
 
-fs.readFile(filePath, 'utf8', function (err, data) {
+fs.writeFile(filePath, content, 'utf8', function (err) {
   if (err) {
     console.error(err);
     return;
   }
 
-  console.log(data);
 });
